861de789d150e6e477ecdfd6ad59af1eebbd6b6a
Mauricio Lauffer <lauffer@outlook.com>
2020-07-21 19:42:47 +1000

    ci: Reduces coverage validation

--- karma.conf.js
+++ karma.conf.js
@@ -40,10 +40,10 @@ module.exports = function(config) {
       dir: 'coverage/',
       check: {
         global: {
-          statements: 90,
-          branches: 90,
-          functions: 90,
-          lines: 90
+          statements: 40,
+          branches: 40,
+          functions: 40,
+          lines: 40
         }
       }
     },
