5106a06837d0b211cf8f96b62294dee7c465eeee
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2022-05-14 11:37:28 +1000

    build: Remove tests

--- dist/test-resources/openui5/smartmockserver/testsuite.qunit.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-window.suite = function() {
-  const testSuite = new parent.jsUnitTestSuite(); // eslint-disable-line
-  const contextPath = location.pathname.substring(0, location.pathname.lastIndexOf('/') + 1);
-  testSuite.addTestPage(contextPath + 'unit/unitTests.html');
-  return testSuite;
-};
--- dist/test-resources/openui5/smartmockserver/unit/allTests.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-sap.ui.require([
-  'test/unit/SmartMockServer'
-], function() {});
